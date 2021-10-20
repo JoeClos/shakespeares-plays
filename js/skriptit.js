c73b6719243c148d0efd827f19341550d747a3da
JoeClos <89244648+JoeClos@users.noreply.github.com>
2021-10-20 16:37:33 +0300

    Javascript file completed

---
+++ js/skriptit.js
@@ -0,0 +1,37 @@
+var style = document.getElementById("flex");
+var li1 = document.getElementById("noDisc1");
+var li2 = document.getElementById("noDisc2");
+var li3 = document.getElementById ("noDisc3");
+var firstList = document.getElementById("grey1");
+var secondList = document.getElementById("grey2");
+var thirdList = document.getElementById("grey3");
+var red1 = document.getElementById("red1");
+var red2 = document.getElementById("red2");
+var red3 = document.getElementById("red3");
+var red4 = document.getElementById("red4");
+var greyRow1 = document.getElementById("odd1");
+var greyRow2 = document.getElementById("odd2");
+var greyRow3 = document.getElementById("odd3");
+var greyRow4 = document.getElementById("odd4");
+var greyRow5 = document.getElementById("odd5");
+var footer = document.getElementById("footer");
+
+
+style.style =  "display: flex;", "flex-flow: row;", "flex-wrap: wrap;";
+li1.style = "list-style: none";
+li2.style = "list-style: none";
+li3.style = "list-style: none";
+firstList.style = "background-color: lightgrey; margin: 1rem 1rem 0 0";
+secondList.style = "background-color: lightgrey; margin: 1rem 1rem 0 0";
+thirdList.style = "background-color: lightgrey; margin: 1rem 1rem 0 0";
+red1.style = "text-decoration-style: dotted; color: #7F170E;     font-weight: bold; font-style: italic; ";
+red2.style = "text-decoration-style: dotted; color: #7F170E;     font-weight: bold; font-style: italic;";
+red3.style = "text-decoration-style: dotted; color: #7F170E;     font-weight: bold; font-style: italic;";
+red4.style = "text-decoration-style: dotted; color: #7F170E;     font-weight: bold; font-style: italic;";
+
+greyRow1.style = "background-color: lightgrey";
+greyRow2.style = "background-color: lightgrey";
+greyRow3.style = "background-color: lightgrey";
+greyRow4.style = "background-color: lightgrey";
+greyRow5.style = "background-color: lightgrey";
+footer.style = "padding-top: 5rem";
